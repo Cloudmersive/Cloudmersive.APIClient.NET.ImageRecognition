30483c59bc99028862e1454b714259e4b561f79f
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-08-04 17:18:15 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -9,16 +9,16 @@
 
 
 
-(Get-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj).replace('<Authors>Swagger</Authors>', "<Authors>Cloudmersive</Authors>") | Set-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj
-(Get-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj).replace('<Company>Swagger</Company>', "<Company>Cloudmersive</Company>") | Set-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj
-(Get-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj).replace('<AssemblyTitle>Swagger Library</AssemblyTitle>', "<AssemblyTitle>Cloudmersive Image Recognition and Processing API Client</AssemblyTitle>") | Set-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj
-(Get-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj).replace('<Description>A library generated from a Swagger doc</Description>', "<Description>Image Recognition and Processing APIs let you use Machine Learning to recognize and process images, and also perform useful image modification operations.</Description>") | Set-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj
-(Get-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj).replace('<TargetFramework>net45</TargetFramework>', "<TargetFramework>netcoreapp2.1</TargetFramework>") | Set-Content ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj
+(Get-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj).replace('<Authors>Swagger</Authors>', "<Authors>Cloudmersive</Authors>") | Set-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj
+(Get-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj).replace('<Company>Swagger</Company>', "<Company>Cloudmersive</Company>") | Set-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj
+(Get-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj).replace('<AssemblyTitle>Swagger Library</AssemblyTitle>', "<AssemblyTitle>Cloudmersive Image Recognition and Processing API Client</AssemblyTitle>") | Set-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj
+(Get-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj).replace('<Description>A library generated from a Swagger doc</Description>', "<Description>Image Recognition and Processing APIs let you use Machine Learning to recognize and process images, and also perform useful image modification operations.</Description>") | Set-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj
+(Get-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj).replace('<TargetFramework>net45</TargetFramework>', "<TargetFramework>netcoreapp2.1</TargetFramework>") | Set-Content ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj
 
 
 
 
 
 
-& dotnet build ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj -c Release
-& dotnet pack ./client/src/Cloudmersive.APIClient.NETCore.ImageRecognition/Cloudmersive.APIClient.NETCore.ImageRecognition.csproj -c Release
+& dotnet build ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj -c Release
+& dotnet pack ./client/src/Cloudmersive.APIClient.NET.ImageRecognition/Cloudmersive.APIClient.NET.ImageRecognition.csproj -c Release
